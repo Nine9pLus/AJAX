dd41ea91f21280b33447687d4a8c826df44e4319
ting <shih123584769@gmail.com>
2025-09-02 10:25:33 +0800

    Model done

--- WebFront/Program.cs
+++ WebFront/Program.cs
@@ -1,3 +1,4 @@
+using WebFront.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebFront.Data;
@@ -8,6 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+// ++
+builder.Services.AddDbContext<NorthwindContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("Northwind"));
+});
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
