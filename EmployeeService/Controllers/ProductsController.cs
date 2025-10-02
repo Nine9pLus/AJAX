ed07ca0754a696a2007e71aa96464a2d8a020abc
ting <shih123584769@gmail.com>
2025-10-02 13:53:45 +0800

    20251002

---
+++ EmployeeService/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeService.DTO;
+using EmployeeService.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeService.Controllers
+{
+	[EnableCors("WebFront")]
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		NorthwindContext _context = new NorthwindContext();
+		public ProductsController(NorthwindContext context)
+		{
+			_context= context;
+		}
+
+		// GET: api/Products
+		[HttpGet]
+		public async Task<IEnumerable<ProductDTO>> GetProducts()
+		{
+			return _context.Products
+				.Select(p => new ProductDTO
+				{
+					ProductName = p.ProductName,
+					UnitPrice = p.UnitPrice,
+				});
+		}
+	}
+}
